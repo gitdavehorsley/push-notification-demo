5cb5341a0c3ecc7415a2050a3ed591da3cbfae22
David Horsley <dave@Davids-Mac-mini.local>
2025-02-20 17:08:07 -0500

    fix: update API endpoint to match API Gateway path configuration

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: 'https://p674zxo5d0.execute-api.us-east-1.amazonaws.com/dev/phone'
+    API_ENDPOINT: 'https://p674zxo5d0.execute-api.us-east-1.amazonaws.com/dev/validate'
 };
 
 const currentConfig = config;

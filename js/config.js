cd601c9c8955fa70f10e76e4493432be4180c31e
David Horsley <dave@Davids-Mac-mini.local>
2025-02-21 14:20:25 -0500

    fix: update API endpoint path to /verify

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: 'https://p674zxo5d0.execute-api.us-east-1.amazonaws.com/dev/validate'
+    API_ENDPOINT: 'https://p674zxo5d0.execute-api.us-east-1.amazonaws.com/dev/verify'
 };
 
 const currentConfig = config;
